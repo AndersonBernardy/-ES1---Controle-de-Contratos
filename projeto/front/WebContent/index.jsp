5a3b629cf7f184ea999f325723c4c6df4a510b2c
Victor Hugo Cardoso Mendes <victor_cardoso00@hotmail.com>
2018-12-02 15:57:00 -0200

    upload front-end
    é nois

---
+++ projeto/front/WebContent/index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Inicio</title>
+</head>
+<body>
+	<form id="form" class="form-horizontal" action="cadastro.jsp">
+		<button type="submit" class="btn btn-primary">Cadastrar Contrato</button>
+	</form>
+	<form id="form" class="form-horizontal" action="consulta.jsp">
+		<button type="submit" class="btn btn-secondary">Consulta Contrato</button>
+	</form>
+</body>
+</html>
---
+++ projeto/front/src/ServletCadastro.java
@@ -0,0 +1,64 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginCheck
+ */
+@WebServlet("/ServletCadastro")
+public class ServletCadastro extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ServletCadastro() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String numContrato = request.getParameter("numContrato");
+		String dtEmissao = request.getParameter("dtEmissao");
+		String validadeInicio = request.getParameter("validadeInicio");
+		String validadeFim = request.getParameter("validadeFim");
+		String tipoContrato = request.getParameter("tipoContrato");
+		String nomeCliente = request.getParameter("nomeCliente");
+		String cpfCliente = request.getParameter("cpfCliente");
+		String cnpjCliente = request.getParameter("cnpjCliente");
+		String email = request.getParameter("email");
+		String emailOpcional = request.getParameter("emailOpcional");
+		String telefone = request.getParameter("telefone");
+		String telefoneOpcional = request.getParameter("telefoneOpcional");
+		String cep = request.getParameter("cep");
+		String cidade = request.getParameter("cidade");
+		String bairro = request.getParameter("bairro");
+		String rua = request.getParameter("rua");
+		String numCasa = request.getParameter("numCasa");
+		String descricao = request.getParameter("descricao");
+		String total = request.getParameter("total");
+		
+		System.out.println("Informações");
+		System.out.println(numContrato+" \n"+dtEmissao+" \n"+validadeInicio+" \n"+validadeFim+" \n");
+		System.out.println(tipoContrato+" \n"+nomeCliente+" \n"+cpfCliente+" \n"+cnpjCliente+" \n");
+		System.out.println(email+" \n"+emailOpcional+" \n"+telefone+" \n"+telefoneOpcional+" \n");
+		System.out.println(cep+" \n"+cidade+" \n"+bairro+" \n"+rua+" \n");
+		System.out.println(numCasa+" \n"+descricao+" \n"+total);
+		
+		if(1==1) {
+			response.sendRedirect("sucesso.jsp");
+		}else {
+			response.sendRedirect("falha.jsp");
+		}
+	
+	}
+
+}
